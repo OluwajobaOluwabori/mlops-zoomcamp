752af758198e1ab4333f1ff6c5c7f43f43515858
JobaBori <45173608+OluwajobaOluwabori@users.noreply.github.com>
2025-06-10 12:52:41 +0200

    Add files via upload
    The goal of this project is to create a simple training pipeline, use MLflow to track experiments and register the best model using Prefect as an orchestrator.

---
+++ 03-orchestration/duration-prediction.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+from prefect import task, flow
+import pickle
+from pathlib import Path
+
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.metrics import root_mean_squared_error
+
+import mlflow
+
+mlflow.set_tracking_uri("http://localhost:5000")
+mlflow.set_experiment("nyc-taxi-experiment")
+
+models_folder = Path('models')
+models_folder.mkdir(exist_ok=True)
+
+
+@task(name = 'records', retries=3, retry_delay_seconds =2, log_prints = True)
+def read_dataframe(year, month):
+    url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month:02d}.parquet'
+    df = pd.read_parquet(url)
+    print(f'Total records:',df.shape)
+
+    df['duration'] = df.tpep_dropoff_datetime - df.tpep_pickup_datetime
+    df.duration = df.duration.dt.total_seconds() / 60
+
+    df = df[(df.duration >= 1) & (df.duration <= 60)]
+
+    categorical = ['PULocationID', 'DOLocationID']
+    df[categorical] = df[categorical].astype(str)
+
+    print(df.size)
+    # output_path = f"filtered_data_{year}_{month:02d}.csv"
+    # df.to_csv(output_path, index=False)
+
+    # # Log the file as an artifact
+    # mlflow.log_artifact(output_path, artifact_path="data")
+
+
+    return df
+
+
+@task(name = 'createX')
+def create_X(df, dv=None):
+    categorical = ['PULocationID', 'DOLocationID']
+
+    numerical = ['trip_distance']
+    dicts = df[categorical + numerical].to_dict(orient='records')
+
+    if dv is None:
+        dv = DictVectorizer(sparse=True)
+        X = dv.fit_transform(dicts)
+    else:
+        X = dv.transform(dicts)
+
+    return X, dv
+
+@task(name = 'model', log_prints = True)
+def train_model(X_train, y_train, X_val, y_val, dv):
+    with mlflow.start_run() as run:
+        
+        lr = LinearRegression()
+        lr.fit(X_train, y_train)
+
+        y_pred = lr.predict(X_val)
+
+        
+        rmse = root_mean_squared_error(y_val, y_pred)
+        mlflow.log_metric("rmse", rmse)
+
+        with open("models/preprocessor.b", "wb") as f_out:
+            pickle.dump(dv, f_out)
+        mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
+
+        mlflow.sklearn.log_model(lr, artifact_path="models_mlflow")
+        print(f'Intercelt of the model:', lr.intercept_)
+        return run.info.run_id
+
+@flow(name = 'runflow')
+def run(year, month):
+    df_train = read_dataframe(year=year, month=month)
+
+    next_year = year if month < 12 else year + 1
+    next_month = month + 1 if month < 12 else 1
+    df_val = read_dataframe(year=next_year, month=next_month)
+
+    X_train, dv = create_X(df_train)
+    X_val, _ = create_X(df_val, dv)
+
+    target = 'duration'
+    y_train = df_train[target].values
+    y_val = df_val[target].values
+
+    run_id = train_model(X_train, y_train, X_val, y_val, dv)
+    print(f"MLflow run_id: {run_id}")
+    return run_id
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Train a model to predict taxi trip duration.')
+    parser.add_argument('--year', type=int, required=True, help='Year of the data to train on')
+    parser.add_argument('--month', type=int, required=True, help='Month of the data to train on')
+    args = parser.parse_args()
+
+    run_id = run(year=args.year, month=args.month)
+
+    with open("run_id.txt", "w") as f:
+        f.write(run_id)
